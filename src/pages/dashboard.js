48eb31a0fcb975d3affccb4c5619919e873f7e7a
AtharvaUtekar <atharva.v.utekar@gmail.com>
2022-05-25 12:55:15 +0530

    Dashboard Initial comp commit

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,4 +1,15 @@
 import React from 'react'
+/*Local data imported */
+import { data } from '../data/index'
+
+/*Module used for alerts */
+import swal from 'sweetalert2'
+
+/*Importing components important in page */
+import header from '../components/header'
+import edit from '../components/edit'
+import list from '../components/list'
+import add from '../components/add'
 
 const dashboard = () => {
   return (
